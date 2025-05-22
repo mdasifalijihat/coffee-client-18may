25469e54d4161bf8117a9ca7e29eaa6ddeea673f
mdasifalijihat <mdasifalijihat@gmail.com>
2025-05-22 18:34:18 +0600

    theme add

--- src/components/fixedPgaes/Navbar.jsx
+++ src/components/fixedPgaes/Navbar.jsx
@@ -1,10 +1,27 @@
-import React, { use } from "react";
+import React, { use, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router";
 import { AuthContext } from "../../AuthContext/AuthContext";
+import { IoMdSunny } from "react-icons/io";
+import { LuSunMoon } from "react-icons/lu";
 
 const Navbar = () => {
   const { user, logOutUser } = use(AuthContext);
-  console.log(user);
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : "light"); 
+ 
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document.querySelector('html').setAttribute("data-theme", localTheme)
+  },[theme]); 
+  const handleToggle =(e)=>{
+    if(e.target.checked){
+      setTheme('dark');
+    }else{
+      setTheme('light')
+    }
+  }
+
+
   const links = [
     <>
       <NavLink className={"m-2 text-xl"} to={"/"}>
@@ -24,15 +41,17 @@ const Navbar = () => {
       </NavLink>
     </>,
   ];
-  
-  const HandleLogOutUser =()=>{
-    logOutUser().then(()=>{
-      console.log('user log out successfully');
-      alert('user logout successfully')
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
+
+  const HandleLogOutUser = () => {
+    logOutUser()
+      .then(() => {
+        console.log("user log out successfully");
+        alert("user logout successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
@@ -73,7 +92,11 @@ const Navbar = () => {
         </div>
         <div className="navbar-end">
           {user ? (
-            <> <span className="mr-2 bg-amber-300">{user.email}</span> <button onClick={HandleLogOutUser}> Logout </button> </>
+            <>
+              {" "}
+              <span className="mr-2 bg-amber-300">{user.email}</span>{" "}
+              <button onClick={HandleLogOutUser}> Logout </button>{" "}
+            </>
           ) : (
             <>
               <NavLink className={"m-2 text-xl"} to={"/login"}>
@@ -84,6 +107,21 @@ const Navbar = () => {
               </NavLink>
             </>
           )}
+          <div>
+            <label className="swap swap-rotate">
+              {/* this hidden checkbox controls the state */}
+              <input 
+              onChange={handleToggle}
+              checked={theme === "light" ? false : true}
+               type="checkbox" />
+              <p className="swap-on h-10 w-10 fill-current">
+                <IoMdSunny size={32} />
+              </p>
+              <p className="swap-off h-10 w-10 fill-current">
+                <LuSunMoon size={32} />
+              </p>
+            </label>
+          </div>
         </div>
       </div>
     </div>
